1184db6c759fc04ff810b25acec7cadb2334e33e
Ankit Sharma <sharmankit9593@gmail.com>
2021-01-24 08:32:04 +0530

    Finally Understood Recursion

--- Linked List/LinkedListReverse.cpp
+++ Linked List/LinkedListReverse.cpp
@@ -7,9 +7,9 @@ struct Node{
     Node *next;
     Node(int data){
         this->data = data;
-        next = NULL;
+        next = nullptr;
     }
-}*head  = NULL;
+}*head  = nullptr;
 
 
 //O(n) Time and O(n) Space
@@ -20,11 +20,14 @@ Node *reverse1(Node * head){
 
     vector<int> v;
 
-    for(Node * curr = head; curr!= NULL; curr = curr->next){
+    for(Node * curr = head; curr!= nullptr; curr = curr->next){
+        //pushing data of linked list in the array
         v.push_back(curr->data);
+        
     }
 
-    for(Node *curr = head; curr != NULL; curr = curr->next){
+    for(Node *curr = head; curr != nullptr; curr = curr->next){
+        //put the data back into the Linked List
         curr->data = v.back();
         v.pop_back();
     }
@@ -32,13 +35,12 @@ Node *reverse1(Node * head){
 }
 
 //Using Three Pointer approach -> next, prev and curr:
-void reverse2()
-	{
-		
+void reverse2(){
 		Node* curr = head;
-		Node *prev = NULL, *next = NULL;
+		Node *prev = nullptr;
+        Node *next = nullptr;
 
-		while (curr != NULL) {
+		while (curr != nullptr) {
 			// Store next
 			next = curr->next;
 
@@ -50,13 +52,25 @@ void reverse2()
 			curr = next;
 		}
 		head = prev;
-	}
+}
+
+Node *recursiveReverse1(Node *head){
+     if (head == nullptr || head->next == nullptr) {
+    return head;
+  }
+
+  Node* reversed_list = recursiveReverse1(head->next);
+
+  head->next->next = head;
+  head->next = nullptr;
+  return reversed_list;
+}
 
 
 void print(Node * head){
     Node * curr =head;
     while(curr != NULL){
-        cout<<curr->data<<" ";
+        cout<<curr->data<<"\t";
         curr = curr->next;
     }
 
@@ -64,24 +78,6 @@ void print(Node * head){
 
 int main(int argc, char const *argv[])
 {
-    // int n;
-    // cin>>n; 
-    // int arr[n];
-
-    // for(int i =0; i<n; i++){
-    //     cin>>arr[i];
-    // }
-
-    // Node *head = new Node(arr[0]);
-
-    // Node *last = head;
-    // Node *t;
-
-    // for(int i = 1; i<n; i++){
-    //     t = new Node(arr[i]);
-    //     last ->next = t;
-    //     last = t;
-    // }
 
     head = new Node(10);
     Node *temp1 = new Node(20);
@@ -94,16 +90,17 @@ int main(int argc, char const *argv[])
     temp2->next =  temp3;
     temp3 -> next = temp4;
     
-    
-    reverse2();
-
+    cout<<"Before Reversal"<<"\t"<<": "<<"\n";
     print(head);
-    // Node * curr = head;
+    cout<<"\n";
 
-    // while(curr != NULL){
-    //     cout<<curr->data<<"\t"<<"==>";
-    //     curr = curr->next;
-    // }
+    
+    Node *ret = recursiveReverse1(head);
+
+    cout<<"After Reversal"<<"\t"<<": "<<"\n";
+
+    
+    print(ret);
 
     return 0;
 }
