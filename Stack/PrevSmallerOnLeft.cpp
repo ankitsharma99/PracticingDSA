b3b5f37ae107ae2f0fce12dfdd953623bd1cb193
Ankit Sharma <sharmankit9593@gmail.com>
2021-02-02 13:27:05 +0530

    Added Previous Smaller on Left Code

---
+++ Stack/PrevSmallerOnLeft.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+#define endl "\n"
+#define FastIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+vector<int> PrevSmallerOnLeft(int arr[], int n){
+    vector<int> v;
+    stack<int> st;
+    v.push_back(-1);
+    st.push(arr[0]);
+
+    for(int i = 1; i<n; i++){
+        if( st.size() && arr[i] > st.top())
+            v.push_back(st.top());
+        
+        else if(!st.size()) v.push_back(-1);
+
+        else if(st.size()  && arr[i] < st.top()){
+            while(st.size() && arr[i] < st.top())
+                st.pop();
+            
+            if(!st.size()) v.push_back(-1);
+
+            else {
+                v.push_back(st.top());
+            }
+        }
+        st.push(arr[i]);
+    }
+    return v;
+}
+
+ 
+int main(int argc, char const *argv[])
+{
+     int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+
+
+    vector<int> val = PrevSmallerOnLeft(arr, n);
+
+    for(int i = 0; i<val.size(); i++){
+        cout<<val[i]<<"\t";
+    }
+    return 0;
+}
