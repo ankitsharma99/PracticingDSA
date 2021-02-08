65f657b3ead3e64df01272348ec4acd63fd9f198
Ankit Sharma <sharmankit9593@gmail.com>
2021-02-09 01:17:00 +0530

    Level Order Implementation - 1

--- Tree/LevelOrderTraversal.cpp
+++ Tree/LevelOrderTraversal.cpp
@@ -12,6 +12,8 @@ struct Node{
     }
 };
 
+
+//O(n)
 void LevelOrderTraversal(Node * root){
     queue<Node *> q;
     if(root == nullptr){
@@ -33,6 +35,45 @@ void LevelOrderTraversal(Node * root){
     }
 }
 
+/*
+In this method, we print the levels after adding a NEWLINE after each level. For example, for the tree that we made in main func, we will get the output as:
+10      
+9       12
+7       8       14      18
+69
+
+This method is just an extension of the above printing of line by line method.
+When we add all the nodes in the queue at a particular level, we print a new line and also add a null indicating the end of the level.
+*/
+void LevelLineTraversal(Node * root){
+    queue<Node *> q1;
+    if(root == nullptr) return;
+
+    q1.push(root);   //add root to queue
+    q1.push(nullptr); //push null value to queue indicating end of the level
+
+    while(q1.size() > 1){    //q1.size should be greater than one as in the end, we will have a null value in the queue
+
+        Node *curr = q1.front();    
+        q1.pop();
+        if(curr == nullptr){         //If curr is pointing to nullptr, print a new line and push a null pointer in the queue, indicating the end of a level
+            cout<<"\n";
+            q1.push(nullptr);
+            continue;          //Important statement so that the loop executes the next iteration
+        }
+
+
+        cout<<curr->data<<"\t";
+        if(curr->left != nullptr){
+            q1.push(curr->left);
+        }
+        if(curr->right != nullptr){
+            q1.push(curr->right);
+        }
+
+    }
+}
+
 int main(int argc, char const *argv[])
 {
 
@@ -47,7 +88,7 @@ int main(int argc, char const *argv[])
     root->right->right->left = new Node(69);
 
 
-    LevelOrderTraversal(root);
+    LevelLineTraversal(root);
     
 
 }
