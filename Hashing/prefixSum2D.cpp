c342fe5e1d1e8cb1347a3a8ea5637ae34d25e94c
Ankit Sharma <sharmankit9593@gmail.com>
2021-02-12 22:18:29 +0530

    added prefix summ in 2D

---
+++ Hashing/prefixSum2D.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+
+/*
+given 2D array of N*N integers. Givem Q queries and in each query given a, b, c, d
+print sum of squares represented by (a, b) as top left point and (c, d) as bottom right point
+*/
+
+using namespace std;
+const int N = 1e7+10;
+int main(int argc, char const *argv[])
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+
+		freopen("output.txt", "w", stdout);
+	#endif // ONLINE_JUDGE
+
+
+	return 0;
+}
