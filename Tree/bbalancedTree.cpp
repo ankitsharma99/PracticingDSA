c758bb9fbdf79a151dc2dfab432cf4377985eb44
Ankit Sharma <sharmankit9593@gmail.com>
2021-02-16 23:54:59 +0530

    Trees practice

--- Tree/bbalancedTree.cpp
+++ Tree/bbalancedTree.cpp
@@ -17,17 +17,19 @@ int isBalanced(Node *root){
     //Check if the node points to nullptr
     if(root == nullptr) return 0;
 
-    //Take a leftheight variable that stores the value returned by the left subtree
+    //Take a leftheight variable that stores the value returned by the left subtree.
+    //Take a right Height variable that stoes the value returned by the right subtree.
+    //?????? IMPORTANT STEP ?????   if |leftSubtree - rightSubtree| is greater than 1, return -1; coz a balanced tree should have difference between left and
+    //right subtree as almost one 
     int leftHeigh = isBalanced(root->left);
 
     if(leftHeigh == -1) return -1;
 
-    //Take a right Height variable that stoes the value returned by the right subtree
+    
     int rightHeight = isBalanced(root->right);
     if(rightHeight == -1) return -1;
 
-    //?????? IMPORTANT STEP ?????   if |leftSubtree - rightSubtree| is greater than 1, return -1; coz a balanced tree should have difference between left and
-    //right subtree as almost one 
+    
     if((abs)(leftHeigh- rightHeight) > 1) return -1;
 
     return max(leftHeigh, rightHeight) + 1;
